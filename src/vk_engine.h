59d4c66067ec748d9516f5827bc8d724b8ae3476
root-kawaii <teo.regge.99@gmail.com>
2023-10-10 01:53:43 +0900

    init

---
+++ src/vk_engine.h
@@ -0,0 +1,71 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <vk_types.h>
+
+#include <vector>
+
+class VulkanEngine
+{
+
+public:
+	// --- omitted ---
+	VkInstance _instance;					   // Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU;			   // GPU chosen as the default device
+	VkDevice _device;						   // Vulkan device for commands
+	VkSurfaceKHR _surface;					   // Vulkan window surface
+	bool _isInitialized{false};
+	int _frameNumber{0};
+	// --- other code ---
+	VkSwapchainKHR _swapchain; // from other articles
+
+	// image format expected by the windowing system
+	VkFormat _swapchainImageFormat;
+
+	// array of images from the swapchain
+	std::vector<VkImage> _swapchainImages;
+
+	// array of image-views from the swapchain
+	std::vector<VkImageView> _swapchainImageViews;
+
+	// ---- other code -----
+	VkQueue _graphicsQueue;		   // queue we will submit to
+	uint32_t _graphicsQueueFamily; // family of that queue
+
+	VkCommandPool _commandPool;			// the command pool for our commands
+	VkCommandBuffer _mainCommandBuffer; // the buffer we will record into
+
+	VkRenderPass _renderPass;
+
+	std::vector<VkFramebuffer> _framebuffers;
+
+	VkSemaphore _presentSemaphore, _renderSemaphore;
+	VkFence _renderFence;
+
+	VkExtent2D _windowExtent{800, 400};
+
+	struct SDL_Window *_window{nullptr};
+
+	// initializes everything in the engine
+	void init();
+
+	// shuts down the engine
+	void cleanup();
+
+	// draw loop
+	void draw();
+
+	// run main loop
+	void run();
+
+private:
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_default_renderpass();
+	void init_framebuffers();
+	void init_sync_structures();
+};
